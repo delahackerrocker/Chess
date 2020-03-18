63d82aad5a3f02ef41dac49c9594f6b270799dae
Dela <stevedelatorre@gmail.com>
2020-03-18 09:53:59 -0400

    Create Knight.java
    Knight now extends the Piece parent class.

---
+++ src/com/tts/chess/Knight.java
@@ -0,0 +1,23 @@
+package com.tts.chess;
+
+public class Knight extends Piece
+{
+    public Knight(boolean isWhite)
+    {
+        super(isWhite);
+    }
+
+    @Override
+    public boolean canMove(Board board, Square squareStart, Square squareEnd)
+    {
+        // we can't move this piece to a square that has a piece of the same color
+        if (squareEnd.getPiece().isWhite() == this.isWhite())
+        {
+            return false;
+        }
+        
+        int x = Math.abs(squareStart.getX() - squareEnd.getX());
+        int y = Math.abs(squareStart.getY() - squareEnd.getY());
+        return ((x * y) == 2);
+    }
+}
